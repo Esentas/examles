4f7f2c63ff0c78c51c9ecb9a7d995607e5bb028f
esentas <bo.evoi@yandex.ru>
2022-06-23 17:11:22 +0500

    add example005

---
+++ example005_ifelse/Program.cs
@@ -0,0 +1,9 @@
+﻿System.Console.Write("input name user: ");
+string username = Console.ReadLine();
+if(username.ToLower() == "masha"){
+    System.Console.WriteLine("Aw, this is Masha!");
+}
+else{
+    System.Console.Write("Hello, ");
+    System.Console.WriteLine(username);
+}
